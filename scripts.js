624d219e647864427ec4e952d542be33020be8f7
Marcin Czekala <marcin_czekala@icloud.com>
2018-05-31 15:11:58 +0200

    add button

--- scripts.js
+++ scripts.js
@@ -2,10 +2,16 @@
 
 var list = document.getElementById('list');
 var add = document.getElementById('addElem');
+var rm = document.getElementById('rmElem');
 
 add.addEventListener('click', function() {
   var element = document.createElement('li');
   var number = document.getElementsByTagName('li').length;
   element.innerHTML = 'item ' + number ;
   list.appendChild(element)
+});
+
+rm.addEventListener('click', function() {
+	var element = document.getElementsByTagName('li')
+	list.removeChild(element[element.length-1])
 });
